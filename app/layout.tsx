d3ba47190966a7da59a74e9370a60c7b48537ce3
jainex17 <jainexp017@gmail.com>
2023-09-10 18:19:29 +0530

    Initialized project and configured dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from "next-themes"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
