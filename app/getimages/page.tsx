5c52d1af951e43280734700d1065cf9ed0a38ed9
jainex17 <jainexp017@gmail.com>
2024-08-17 16:04:13 +0530

    refactor: Improve image downloading functionality and UI

--- app/getimages/page.tsx
+++ app/getimages/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { fetchImages } from "@/utils/fetchImages";
-const FileSaver = require('file-saver');
 
 interface Image {
   fileName: string;
@@ -19,15 +18,8 @@ export default function Page() {
     setImages(imageurls.map(([fileName, url]) => ({ fileName, url })));
   }
 
-  function downloadImage(url: string, fileName: string) {
-    FileSaver.saveAs(url, fileName);
-    // const link = document.createElement("a");
-    // link.href = url;
-    
-    // link.download = url.split("/").pop() || "image";
-    // document.body.appendChild(link);
-    // link.click();
-    // document.body.removeChild(link);
+  async function downloadImage(url: string) {  
+    window.open(url);
   }
 
   return (
@@ -40,10 +32,10 @@ export default function Page() {
             placeholder="Enter Secret Code"
             value={secretcode}
             onChange={(e) => setSecretCode(e.target.value)}
-            className="mr-2 w-full border border-gray-300 dark:border-gray-700 dark:bg-[#333] rounded-sm p-2 text-gray-400"
+            className="mr-2 w-full border border-gray-300 dark:border-gray-700 dark:bg-[#333] p-2 text-gray-400"
           />
           <button
-            className="w-20 px-3 font-semibold dark:bg-[#292218] border border-gray-900 dark:border-[#b47517] dark:hover:bg-[#492218] rounded-sm"
+            className="w-20 px-3 font-semibold dark:bg-[#292218] border border-gray-900 dark:border-[#b47517] dark:hover:bg-[#492218]"
             onClick={getimages}
             disabled= {secretcode.length < 7}
           >
@@ -63,7 +55,7 @@ export default function Page() {
           {images.map((image, index) => (
             <div key={index}>
               <div
-                className="flex flex-col border border-[#b47517] rounded-md select-none"
+                className="flex flex-col border border-[#b47517] select-none relative" 
               >
                 <img
                   src={image.url}
@@ -71,20 +63,24 @@ export default function Page() {
                   draggable={false}
                   className="hover:brightness-75 transition  flex justify-center items-center object-cover w-64 h-64"
                 />
-                <div className="flex justify-between bg-white dark:bg-[#333]">
-                  <p className="text-gray-600 dark:text-white text-xs py-2 px-3">
+                <div className="absolute dark:bg-gray-800 bg-gray-200 right-1 top-1 rounded-sm cursor-pointer p-1"
+                  onClick={()=>downloadImage(image.url)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-5"><path fill="currentColor" d="m12 16l-5-5l1.4-1.45l2.6 2.6V4h2v8.15l2.6-2.6L17 11zm-6 4q-.825 0-1.412-.587T4 18v-3h2v3h12v-3h2v3q0 .825-.587 1.413T18 20z"></path></svg>
+                </div>
+                <div className="flex justify-between dark:bg-[#333] bg-gray-800 text-gray-300">
+                  <p className="text-gray-300 dark:text-white text-xs py-2 px-3">
                     {image.fileName && image.fileName.length > 30
-                      ? image.fileName.slice(0, 30) + "..."
+                      ? image.fileName.slice(0, 45) + "..."
                       : image.fileName}
                   </p>
                 </div>
               </div>
-              <button
+              {/* <button
                 onClick={() => downloadImage(image.url, image.fileName)}
-                className="mt-2 p-2 dark:bg-gray-700 dark:text-gray-300 bg-gray-100 text-gray-900 rounded-md w-full"
+                className="mt-2 p-2 dark:bg-gray-700 dark:text-gray-300 bg-gray-800 text-gray-300 rounded-md w-full"
               >
                 Download
-              </button>
+              </button> */}
             </div>
           ))}
         </div>
