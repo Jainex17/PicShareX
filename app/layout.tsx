96914bf8e68f5d915fad60d8ebf6f8be24a17fd4
jainex17 <jainexp017@gmail.com>
2024-08-18 15:59:43 +0530

    refactor: Add meta tags and favicon to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,12 @@ export default function RootLayout({
   
   return (
     <html lang="en">
+    <head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>PicShareX</title>
+      <link rel="icon" href="/favicon.ico" />
+    </head>
       <body className={"dark"}>
       <Navbar />
       <main className="flex gap-5 bg-gray-100 dark:bg-[#26262a] flex-col md:flex-row">
